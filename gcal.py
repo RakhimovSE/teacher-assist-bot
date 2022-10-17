bf0f24622a4ed290b22bffd468978d3db31b90aa
Sevastyan Rakhimov <Rakhimov.SE@gmail.com>
2022-10-18 02:18:04 +0300

    Move google calendar consts to .env

--- gcal.py
+++ gcal.py
@@ -14,10 +14,7 @@ env = dotenv_values()
 
 
 class GCal:
-    TOKEN_FILE = 'token.json'
-    CREDS_FILE = 'credentials.json'
-    # If modifying these scopes, delete the file token.json.
-    SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+    SCOPES = env['GCAL_SCOPES'].split(';')
 
     def __init__(self, max_results=10):
         self.max_results = max_results
@@ -32,7 +29,7 @@ class GCal:
             # Call the Calendar API
             now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
             print('Getting the upcoming 10 events')
-            events_result = self.__service.events().list(calendarId=env['CALENDAR_ID'], timeMin=now,
+            events_result = self.__service.events().list(calendarId=env['GCAL_ID'], timeMin=now,
                                                          maxResults=self.max_results,
                                                          singleEvents=True,
                                                          orderBy='startTime').execute()
@@ -48,18 +45,18 @@ class GCal:
         # The file token.json stores the user's access and refresh tokens, and is
         # created automatically when the authorization flow completes for the first
         # time.
-        if os.path.exists(GCal.TOKEN_FILE):
-            _creds = Credentials.from_authorized_user_file(GCal.TOKEN_FILE, GCal.SCOPES)
+        if os.path.exists(env['GCAL_TOKEN_FILE']):
+            _creds = Credentials.from_authorized_user_file(env['GCAL_TOKEN_FILE'], GCal.SCOPES)
         # If there are no (valid) credentials available, let the user log in.
         if not _creds or not _creds.valid:
             if _creds and _creds.expired and _creds.refresh_token:
                 _creds.refresh(Request())
             else:
                 flow = InstalledAppFlow.from_client_secrets_file(
-                    GCal.CREDS_FILE, GCal.SCOPES)
+                    env['GCAL_CREDS_FILE'], GCal.SCOPES)
                 _creds = flow.run_local_server(port=0)
             # Save the credentials for the next run
-            with open(GCal.TOKEN_FILE, 'w') as token:
+            with open(env['GCAL_TOKEN_FILE'], 'w') as token:
                 token.write(_creds.to_json())
         return _creds
 
