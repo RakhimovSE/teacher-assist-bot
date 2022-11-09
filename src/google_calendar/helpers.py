6f3759a3fb5bc659b5e52a27f2e16075634f59fb
Sevastyan Rakhimov <Rakhimov.SE@gmail.com>
2022-11-10 01:09:23 +0300

    Create get_reminder_text function

--- src/google_calendar/helpers.py
+++ src/google_calendar/helpers.py
@@ -1,4 +1,8 @@
 import re
+from datetime import datetime
+
+import pytz
+from babel.dates import get_timezone_location, format_date, format_time, format_timedelta
 
 from gcsa.event import Event
 
@@ -16,3 +20,19 @@ def get_args(event: Event):
         if len(parts) == 2 and is_match(parts[0]):
             args[parts[0]] = parts[1]
     return args
+
+
+def get_reminder_text(event: Event, locale='ru_RU', is_formal=True):
+    event_tz = pytz.timezone(event.timezone)
+    start_dt = event.start.replace(tzinfo=None).astimezone(event_tz)
+    now = datetime.now(tz=event_tz)
+    day = format_timedelta(start_dt - now, add_direction=True, locale=locale)
+    date = format_date(start_dt, format='d MMMM', locale=locale)
+    time = format_time(start_dt, format='short', locale=locale)
+    tz_city = get_timezone_location(event_tz, locale=locale, return_city=True)
+    lines = [
+        'Добрый день!' if is_formal else 'Привет!',
+        f'У нас по плану занятие {day} ({date} в {time}, время {tz_city})',
+        'Всё в силе?',
+    ]
+    return '\n'.join(lines)
